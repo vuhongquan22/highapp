6a3ccd5d8f422fafe7fcddb3d49fea958851bb10
vuhongquan <quanvh62@wru.vn>
2020-11-19 22:21:11 +0700

    sửa lại api call giá đất phường

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,30 +1,14 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>New</Text>
+  return(
+    <View>
+        <Text>New</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
